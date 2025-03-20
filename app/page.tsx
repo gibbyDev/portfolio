dd20e74e210d1af07790e81e29c4b1eee1e238fe
gibbyDev <gibbyDEV@protonmail.com>
2025-03-20 14:33:02 +0000

    I lied, this is the last one

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ const Home = () => {
     <main className="relative bg-black-100 flex overflow-x-hidden overflow-y-hidden top-0 justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        {/* <Hero /> */}
+        <Hero />
         <Grid />
-        <Clients />
+        {/* <Clients /> */}
         <Footer />
       </div>
     </main>
--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a react component library",
+    title: "Currently building a robust modular CRM backend",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
