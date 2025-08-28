00994aeeccc3db85b2c6cd06636d0e4b56121ad7
gibbyDev <gibbyDEV@protonmail.com>
2025-08-28 10:21:40 -0400

    Meaningful Commit Message

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Intro() {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="h-full w-full flex items-center justify-center m-0 p-0">
       <iframe
-        className="w-[85%] h-[620px] md:w-[85%] md:h-[620px] sm:w-full sm:h-[220px] rounded-xl"
+        className="w-full h-[220px] md:w-[85vw] md:h-[520px] lg:w-[85%] lg:h-[620px] rounded-xl"
         src="https://www.youtube.com/embed/rnVhezZaXdk?controls=0&modestbranding=1&rel=0&showinfo=0"
         title="YouTube video player"
         frameBorder="0"
