64a8f54ec7de1451a641f12152a859c9d7e1f83b
gibbyDev <gibbyDEV@protonmail.com>
2025-08-29 11:15:56 -0400

    Meaningful Commit Message

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -5,12 +5,12 @@ export default function Intro() {
     <div className="h-full w-full flex items-center justify-center m-0 p-0">
       <iframe
         className="w-full h-[220px] md:w-[85vw] md:h-[520px] lg:w-[85%] lg:h-[620px] rounded-xl"
-        src="https://www.youtube.com/embed/rnVhezZaXdk?controls=0&modestbranding=1&rel=0&showinfo=0"
+        src="https://www.youtube.com/embed/rnVhezZaXdk?"
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
     </div>
   );
