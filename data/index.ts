73a27edac902d26a2c0cf873212c3b7dee27f98a
gibbyDev <gibbyDEV@protonmail.com>
2025-08-29 13:08:53 -0400

    Meaningful Commit Message

--- data/index.ts
+++ data/index.ts
@@ -12,7 +12,7 @@ export const gridItems = [
     className:
       "z-10 lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[70vh] bg-gradient-to-tr from-black to-transparent",
     imgClassName:
-      "z-50 w-full h-full bg-gradient-to-t from-black/70 via-transparent to-transparent",
+      "z-50 w-full h-full bg-gradient-to-tr from-black via-transparent to-transparent",
     titleClassName: "justify-end",
     img: "/portfolio/me.png",
     spareImg: "",
